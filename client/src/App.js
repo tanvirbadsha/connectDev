2e85aeeb57a90d0deaae0d1c93b17ade1ab022b1
Tanvir Badsha <mstanvir55@gmail.com>
2023-08-17 19:33:20 +0600

    cleaned up and created some initial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import  Landing  from './components/layouts/Landing';
+import  Navbar  from './components/layouts/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>  {/* fragments er shorter version <Fragment> */}
+        <Navbar />
+        <Routes>
+          <Route  path="/" element={< Landing />} />
+        </Routes>
+        <section className="container">
+            <Routes>
+              <Route  path="/register" element={< Register />} />
+              <Route  path="/login" element={< Login />} />
+            </Routes>
+        </section>
+      </>
+    </Router>
   );
 }
 
